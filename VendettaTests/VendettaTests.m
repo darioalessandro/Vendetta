df3fbf1bf086ac5388581fcd6a7e44c64fd441c9
Dario Lencina <darioalessandro@me.com>
2012-11-29 20:34:57 -0500

    Initial Commit

---
+++ VendettaTests/VendettaTests.m
@@ -0,0 +1,32 @@
+//
+//  VendettaTests.m
+//  VendettaTests
+//
+//  Created by Dario Lencina on 11/29/12.
+//  Copyright (c) 2012 Dario Lencina. All rights reserved.
+//
+
+#import "VendettaTests.h"
+
+@implementation VendettaTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in VendettaTests");
+}
+
+@end
