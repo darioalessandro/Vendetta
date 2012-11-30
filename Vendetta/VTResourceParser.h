98c8ad98ec4b3425528044f52995c45af9f7ea90
Dario Lencina <darioalessandro@me.com>
2012-11-30 02:11:10 -0500

    Changed The ResourceParser stuff.

--- Vendetta/VTResourceParser.h
+++ Vendetta/VTResourceParser.h
@@ -8,8 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-@interface VTResourceParser : NSObject
-
+@interface VTResourceParser : NSObject 
 -(NSArray *)URLsFromData:(NSData *)data;
 
 @end
--- Vendetta/VTResourceParser.m
+++ Vendetta/VTResourceParser.m
@@ -11,9 +11,23 @@
 
 @implementation VTResourceParser
 
+#pragma mark -
+#pragma mark Public
+
 -(NSArray *)URLsFromData:(NSData *)data{
     NSString * xPathQuery= @"//*[@src]/@src | //*[@href]/@href";
+    NSDate * start=[NSDate date];
     NSArray * nodes= PerformHTMLXPathQuery(data, xPathQuery);
+    NSDate * end=[NSDate date];
+    NSLog(@"parsing time %lf", [end timeIntervalSinceDate:start]);
+    NSArray * urls=[self URLsFromRawXPathQueryResponse:nodes];
+    return urls;
+}
+
+#pragma mark -
+#pragma mark Private
+
+-(NSArray *) URLsFromRawXPathQueryResponse:(NSArray *)nodes{
     NSMutableArray * array=[NSMutableArray array];
     [nodes enumerateObjectsUsingBlock:^(NSDictionary * obj, NSUInteger idx, BOOL *stop) {
         NSURL * url=[NSURL URLWithString:[obj objectForKey:@"nodeContent"]];
@@ -22,5 +36,6 @@
     }];
     return array;
 }
+
 
 @end
